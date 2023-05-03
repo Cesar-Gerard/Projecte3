7cc1f50cf0c7c32defdd07d790cbc977a173f3fb
Gerard <geradcesar428@gmail.com>
2023-05-03 19:21:44 +0200

    NAvigation_Funciona

--- Projecte_3/app/src/main/java/com/example/dietaapp/CurrentPlanFragment.java
+++ Projecte_3/app/src/main/java/com/example/dietaapp/CurrentPlanFragment.java
@@ -36,11 +36,14 @@ public class CurrentPlanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_current_plan, container, false);
+        View v = inflater.inflate(R.layout.fragment_current_plan, container, false);
 
-        //Connectem elements
+        edtPes = v.findViewById(R.id.edtPes);
+        edtPes.setText(info.getName_User());
 
+        return v;
 
     }
 }
