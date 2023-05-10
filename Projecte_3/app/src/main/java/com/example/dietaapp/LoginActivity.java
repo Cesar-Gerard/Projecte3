52bbc7a7a0ba080a4ddc79e8c2d3aadf8f3627c8
Gerard <geradcesar428@gmail.com>
2023-05-10 21:22:15 +0200

    2023/05/10

--- Projecte_3/app/src/main/java/com/example/dietaapp/LoginActivity.java
+++ Projecte_3/app/src/main/java/com/example/dietaapp/LoginActivity.java
@@ -5,14 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-import com.example.dietaapp.databinding.ActivityMainBinding;
+import android.widget.Toast;
 import com.example.dietaapp.databinding.LoginPageBinding;
-
 import api.ApiManager;
 import model.LoginResponse;
 import model.User_Retro;
@@ -33,40 +27,44 @@ public class LoginActivity extends AppCompatActivity{
 
         //--------------------------------//
 
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-        programarBotons(queue);
+        programarBotons();
 
     }
 
 
-    private void programarBotons(final RequestQueue queue) {
+    private void programarBotons() {
+
+
         binding.btnLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
 
                     binding.progressBar.setVisibility(View.VISIBLE);
+                    ApiManager.getInstance().login(binding.edtName.getText().toString(), binding.edtPassword.getText().toString(), new Callback<LoginResponse>() {
 
-                ApiManager.getInstance().login("gcesar", "2003", new Callback<LoginResponse>() {
+                        @Override
+                        public void onResponse(Call<LoginResponse> call, retrofit2.Response<LoginResponse> response) {
+                            
+                            if(response.code()>=200 && response.code()<=300) {
 
-                    @Override
-                    public void onResponse(Call<LoginResponse> call, retrofit2.Response<LoginResponse> response) {
+                                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                                User_Retro.setUser(response.body().getData().getUser());
+                                User_Retro.setToken(response.body().getData().getToken());
 
-                        Intent i = new Intent(LoginActivity.this,MainActivity.class);
-                        User_Retro.setUser(response.body().getData().getUser());
-                        User_Retro.setToken(response.body().getData().getToken());
-                        //binding.progressBar.setVisibility(View.INVISIBLE);
-                        startActivity(i);
+                                startActivity(i);
 
+                            }else{
+                                cridaError();
+                            }
 
-                    }
+                        }
 
-                    @Override
-                    public void onFailure(Call<LoginResponse> call, Throwable t) {
-                        binding.edtName.setText("error");
-                    }
-                });
+                        @Override
+                        public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+                        }
+                    });
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -74,5 +72,9 @@ public class LoginActivity extends AppCompatActivity{
 
             }
         });
+    }
+
+    private void cridaError() {
+        Toast.makeText(LoginActivity.this, "Nom o contrasenya incorrectes",Toast.LENGTH_LONG).show();
     }
 }
